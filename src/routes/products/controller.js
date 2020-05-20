60d6b2ac61da30c77f6495f6827d8582bbbf9342
wilantury <ventas@antury.net>
2020-05-20 14:46:12 -0500

    [add] add controller to get a produt, list products

--- src/routes/products/controller.js
+++ src/routes/products/controller.js
@@ -7,78 +7,128 @@ const TABLA_PRODUCTS = 'products';
 const TABLA_ALBUMS = 'albums';
 const TABLA_PHOTOS = 'product_photos';
 
-function controller(injectedStore){
+function controllerProducts(injectedStore){
     let store = injectedStore;
     if (!store) {
         store = require('../../store/mysql');
     }
-
-    function controller(injectedStore){
-        let store = injectedStore;
-        if (!store) {
-            store = require('../../store/mysql');
+    /**
+     * Logic to insert a Product into the store.
+     * @param {Object} body - The Product information 
+     * @returns {Promise<object[]>} res - result of Product insertion
+     */
+    async function insertProduct(body) {
+        const product = {
+            description: body.description,
+            product_title: body.title,
+            cost: body.cost,
+            quantity: body.quantity,
+            creation_date: new Date(),
+            id_seller: '123',
+            available: 1,
+            id_countries:'123',
+            id_categories:body.id_categories,
+            score: 0,
         }
-        /**
-         * Logic to insert a Product into the store.
-         * @param {Object} body - The Product information 
-         * @returns {Promise<object[]>} res - result of Product insertion
-         */
-        async function insertProduct(body) {
-            const product = {
-                description: body.description,
-                product_title: body.title,
-                cost: body.cost,
-                quantity: body.quantity,
-                creation_date: new Date(),
-                id_seller: '123',
-                available: 1,
-                id_countries:'123',
-                id_categories:body.id_categories,
-                score: 0,
+        product.id_products = nanoid();
+
+        const album = {
+            id_albums: nanoid(), 
+            id_products: product.id_products,
+            description: body.title,
+            created_date: new Date()
+        }
+        product.id_albums = album.id_albums;
+        try{
+            await store.insert(TABLA_ALBUMS, album);
+        }catch(err){
+            throw err;
+        }
+        
+        if(body.photo){
+            const photo = {
+                id_product_photos: nanoid(),
+                description: body.photo.description,
+                url_photo: body.photo.url,
+                id_albums: album.id_albums,
+                created_date: new Date(),
+                visible: true,
             }
-            product.id_products = nanoid();
-    
-            const album = {
-                id_albums: nanoid(), 
-                id_products: product.id_products,
-                description: body.title,
-                created_date: new Date()
-            }
-            product.id_albums = album.id_albums;
             try{
-                await store.insert(TABLA_ALBUMS, album);
+                await store.insert(TABLA_PHOTOS, photo);
             }catch(err){
                 throw err;
             }
-            
-            if(body.photo){
-                const photo = {
-                    id_product_photos: nanoid(),
-                    description: body.photo.description,
-                    url_photo: body.photo.url,
-                    id_albums: album.id_albums,
-                    created_date: new Date(),
-                    visible: true,
-                }
-                try{
-                    await store.insert(TABLA_PHOTOS, photo);
-                }catch(err){
-                    throw err;
-                }
-            }
-                return await store.insert(TABLA_PRODUCTS, product);
         }
+            return await store.insert(TABLA_PRODUCTS, product);
+    }
+
+    /**
+     * Logic to update a product
+     * @method PUT 
+     * @param {Object} body - The Product information to be updated
+     * @returns {Object} res - result of Product update operation
+    */
+    async function updateProduct(body) {
+        /**
+         * @const {Object} - object to insert into product table
+         */
+        const product = {
+            description: body.description,
+            product_title: body.title,
+            cost: body.cost,
+            quantity: body.quantity,
+            id_seller: '123',
+            available: body.available,
+            id_countries:'123',
+            id_categories:body.id_categories,
+        }
+
+        if(body.photo){
+            /**
+             * @const {Object} - object to insert into photo table
+             */
+            const photo = {
+                description: body.photo.description,
+                url_photo: body.photo.url,
+                visible: body.photo.visible,
+            }
+            const queryUpdatePhoto = `UPDATE ${TABLA_PHOTOS} SET ? WHERE id_albums=(SELECT id_albums FROM ${TABLA_ALBUMS} WHERE albums.id_products='${body.id_products}')`;
+            try{
+                await store.update(queryUpdatePhoto, photo);
+            }catch(err){
+                throw err;
+            }
+        }
+
+        const queryUpdateProductInfo = `UPDATE ${TABLA_PRODUCTS} SET ? WHERE id_products='${body.id_products}'`;
+        return await store.update(queryUpdateProductInfo, product);
+    }
+
+    /**
+     * Logic to list all Products of the store.
+     * @returns {Promise<object[]>} res - List of Products
+     */
+    async function listProducts(){
+        return await store.list(TABLA_PRODUCTS);
+    }
+
+    /**
+     * Logic to get one Product with a product ID target.
+     * @param {string} id - The product ID target 
+     * @returns {Promise<object[]>} res - result of one Product
+     */
+    async function getProduct(id){
+        const query = `SELECT * FROM ${TABLA_PRODUCTS} WHERE id_products='${id}'`;
+        return await store.get(query);
+    }
 
     return {
         insertProduct,
-        update,
-        list,
-        get,
-        getProductByName,
-        getProductByPrice,
-        getProductsByCategory,
-        getLatest,
+        updateProduct,
+        listProducts,
+        getProduct,
     }
 }
 
-module.exports = controller;
+module.exports = controllerProducts;
