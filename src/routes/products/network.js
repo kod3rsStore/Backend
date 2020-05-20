23a6145c12d5b5a5f42f7ffe448066f8635a348d
wilantury <ventas@antury.net>
2020-05-20 14:28:42 -0500

    [add] list produts and get a product with an ID_product

--- src/routes/products/network.js
+++ src/routes/products/network.js
@@ -12,12 +12,12 @@ const ControllerProducts = require('./index')
  */
 router.post('/', insertProduct);
 router.put('/', updateProduct);
-router.get('/', list);
+router.get('/', listProducts);
 router.get('/latest', latest);
 router.get('/search/name', searchByName);
 router.get('/search/price', searchByPrice);
 router.get('/search/category', searchByCategory);
-router.get('/:id', get);
+router.get('/:id', getProduct);
 
 /**
  * API Endpoint to insert a Product in the data base.
@@ -41,9 +41,37 @@ async function insertProduct(req, res, next){
  */
 async function updateProduct(req, res, next){
     try{
-        const resUpdateProduct = await Controller.updateProduct(req.body);
+        const resUpdateProduct = await ControllerProducts.updateProduct(req.body);
         response.success(req, res, resUpdateProduct, 200);
     }catch(err){
         response.error(req, res, err.message, 500, 'error network Products Update');
+    }
+}
+
+/**
+ * API Endpoint to list all Products from the data base.
+ * @method GET
+ * @returns {<Object[]>} res - list of Products
+ */
+async function listProducts(req, res, next){
+    try{
+        const resListProducts = await ControllerProducts.listProducts();
+        response.success(req, res, resListProducts, 200);
+    }catch(err){
+        response.error(req, res, err.message, 500, 'error network Products');
+    }
+}
+/**
+* API Endpoint to get a Product with a product ID target.
+* @method GET 
+* @param {params} req - The Product ID 
+* @returns {<Object[]>} res - Product
+*/
+async function getProduct(req, res, next){
+    try{
+        const resGetProduct = await Controller.getProduct(req.params.id);
+        response.success(req, res, resGetProduct, 200);
+    }catch(err){
+        response.error(req, res, err.message, 500, 'error network Products');
     }
 }
