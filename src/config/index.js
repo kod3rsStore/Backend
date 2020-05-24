85d291c9df89573c13f3fab74b59458d2a4637a5
wilantury <ventas@antury.net>
2020-05-23 19:10:17 -0500

    [update] .env file

--- src/config/index.js
+++ src/config/index.js
@@ -7,8 +7,7 @@ const config = {
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
         database: process.env.DB_NAME,
-        port: process.env.DB_PORT,
-        flags: process.env.DB_FLAGS
+        port: process.env.DBPORT,
       },
 }
 
