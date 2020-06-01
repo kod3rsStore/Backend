e0ddbc9c8f6b278a6ab46f752f099767628ec3b7
luisrdz5 <luis_rdz5@hotmail.com>
2020-05-31 22:15:49 -0500

    added the documentation container

---
+++ internaldocs/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(`API Internal Documentation v 0.01`);
+  });
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
