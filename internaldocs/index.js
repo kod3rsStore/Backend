d80b1d681bc316ff51cdbb020668f365a9c30b4f
luisrdz5 <luis_rdz5@hotmail.com>
2020-05-31 22:22:31 -0500

    addedd the internal documentation

--- internaldocs/index.js
+++ internaldocs/index.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+/*
 app.get('/', (req, res) => {
     res.send(`API Internal Documentation v 0.01`);
   });
-
+  */
+app.use('/', express.static(__dirname + '/public'));
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
